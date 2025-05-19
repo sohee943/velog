87518a9415691d2c7665e6a52ae0a778a49a7206
soehe <sohee943lim@gmail.com>
2025-05-19 20:04:19 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -3,7 +3,7 @@ import git
 import os
 
 # 벨로그 RSS 피드 URL
-# example : rss_url = 'https://api.velog.io/rss/@rimgosu'
+# example : rss_url = 'https://api.velog.io/rss/@rimgosu' test
 rss_url = 'https://api.velog.io/rss/@ssohopeful'
 
 # 깃허브 레포지토리 경로
